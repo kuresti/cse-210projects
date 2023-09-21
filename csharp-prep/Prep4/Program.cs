4081aedd570186ebad6c4f3e560088a0207e17c2
kuresti <ure17002@byui.edu>
2023-09-20 21:12:07 -0600

    finished stretch exercises on prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -15,7 +15,7 @@ class Program
 
         //Initializing variables
         int number = -1;
-        string entry = "";
+        
 
         //While loop to add user input to numbers list
         while (number != 0)
@@ -23,11 +23,16 @@ class Program
         
         //Prompt to add numbers to list
         Console.Write("Enter number: ");
-        entry = Console.ReadLine();
-        number = int.Parse(entry);
+        
+        
+        number = int.Parse(Console.ReadLine());
 
         //Adding user input number to numbers list
+        //Excluding 0 from the numbers list
+        if (number != 0)
+        {
         numbers.Add(number);
+        }
         }
         //The sum of numbers list using the Sum() method
         int total = numbers.Sum();
@@ -42,21 +47,21 @@ class Program
         Console.WriteLine($"The largest number is: {max}");
 
         //The smallest postitive number in numbers list
-        numbers.Sort();
-       
-       Console.WriteLine("The sorted list is:");
-        
-        //foreach loop to iterate through list sorting list by number
-        foreach (int x in numbers)
+        foreach (int num in numbers)
         {
-            if (x != 0)
-            Console.WriteLine(x);
+            if(num > 0 && num < max)
+            {
+                max = num;
+            }
         }
-        
-        
+        Console.WriteLine($"The smallest positive number is: {max}");
 
-
-
-        
+        //Sorted list of numbers
+        Console.WriteLine($"The sorted list is: ");
+        numbers.Sort();
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Function that displays message
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        //Call DisplayWelcome Function
+        DisplayWelcome();
+
+        //Function that gets user input and returns
+        //it as a string
+        static void PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+        }
+
+        //Call PromptUserName Function
+        PromptUserName();
+
+        //Functions that gets user's favorite number (as an integer)
+        static void PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            
+
+        }
+
     }
+
 }

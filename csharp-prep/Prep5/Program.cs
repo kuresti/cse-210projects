ff7c467d87c0e1f8e973ce762f302c8bc738d672
kuresti <ure17002@byui.edu>
2023-09-21 14:15:33 -0600

    Functions prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,37 +2,54 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    //Welcome to program function
+    static void DisplayWelcome()
     {
-        //Function that displays message
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
-
-        //Call DisplayWelcome Function
-        DisplayWelcome();
-
-        //Function that gets user input and returns
-        //it as a string
-        static void PromptUserName()
-        {
-            Console.Write("Please enter your name: ");
-            string userName = Console.ReadLine();
-        }
-
-        //Call PromptUserName Function
-        PromptUserName();
-
-        //Functions that gets user's favorite number (as an integer)
-        static void PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            int favoriteNumber = int.Parse(Console.ReadLine());
-            
-
-        }
-
+        Console.WriteLine("Welcome to the program!");
     }
 
+    //User Name prompt function
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    //Favorite number function
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userNumber = Console.ReadLine();
+        int number = int.Parse(userNumber);
+        return number;
+    }
+
+    //Squared number function
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    //Display result function
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
+    }
+
+    //Calling Main method to call functions
+    static void Main(string[] args)
+    {
+        //Welcome
+        Program.DisplayWelcome();
+
+        //Functions getting prompts from user
+        int favoriteNumber = PromptUserNumber();
+        string name = PromptUserName();
+
+        //Functions that take parameters
+        int numberSquared = SquareNumber(favoriteNumber);
+        Program.DisplayResult(name, numberSquared);
+    }
 }

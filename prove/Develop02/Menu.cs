5d3a3c8dd11ad15d6353415354b694a23d9a3fd5
kuresti <ure17002@byui.edu>
2023-10-17 13:35:23 -0600

    MyJournal

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -5,59 +5,73 @@ public class Menu
 
     //Attribte of Menu Class
 
-    //boolean attribute set to true
-    //public bool _choice =  true;
+    //variable for user choice
+    public string _choice = "";
+
+    //variable to validate user choice
+    public bool _validChoice = false;
 
     //Constructor for Menu
     public Menu()
     {
-    }
+    }    
 
-    //New instance of PromptGenerator Class
-    PromptGenerator newPrompt = new PromptGenerator();
+    //New instance of Journal Class
+    Journal myJournal = new Journal();
 
-    //variable for user choice
-    public string _choice;
-
-
+    
 
      //Behaviors/methods of Menu class
-    //while loop to iterate through menu options for user to choose
+    //while loop to iterate through menu options for user to choose an option
     public void DisplayMenu()
     {
+        do
+        {
+        Console.WriteLine("\nWelcome to My Journal App!");
+        Console.WriteLine("Please choose one of the following menu items: ");        
+        Console.WriteLine("1. Write a journal entry.");
+        Console.WriteLine("2. Load journal from file");
+        Console.WriteLine("3. Save journal to file");
+        Console.WriteLine("4. Display journal entries.");
+        Console.WriteLine("5. Quit program.");
+
+        //Gets the user's choice
+         _choice = Console.ReadLine();
         
-            
-            Console.WriteLine("Please select one of the following choices: ");
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Load");
-            Console.WriteLine("3. Save");
-            Console.WriteLine("4. Display");
-            Console.WriteLine("5. Exit");
-            
-            _choice = Console.ReadLine();
-            
-           switch (_choice)
-           {
-                case "1":                    
-                    newPrompt.DisplayPrompt();
-                    break;
+        //validates user's  choice
+         if (_choice =="1" || _choice == "2" || _choice == "3" || _choice == "4" || _choice == "5")
+         {
+            _validChoice = true;
+         }
+         else
+         {
+            Console.WriteLine("\nYour choice is not valid. Please choose 1-5.\n");
+         }
 
-                case "2":
-
-                case "3":
-
-                case "4":
-                
-                case "5":
-                    ExitProgram();
-                    break;
-
-                
-                
-           }
+         
+        switch (_choice)
+        {
+            case "1":
+               myJournal.AddEntry();
+                break;
             
-            
-        
+            case "2":
+                myJournal.LoadJournal();
+                break;
+
+            case "3":
+                myJournal.SaveToFile();
+                break;
+
+            case "4":
+                myJournal.DisplayJournalEntries();
+                break;
+
+            case "5":
+                break;
+
+        }
+        } while (_choice != "5");
     }
 
     public void ExitProgram()
@@ -66,4 +80,9 @@ public class Menu
         }
     
 }
+    
+            
+            
+
+
   

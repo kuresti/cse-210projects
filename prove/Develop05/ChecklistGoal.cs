c987058ed2d042d8affa11f50bfac470c731f3d1
kuresti <ure17002@byui.edu>
2023-11-17 10:02:49 -0700

    Goals

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -13,8 +13,7 @@ using System;
      //Constructor of the ChecklistGoal class for creating a goal
 
      public ChecklistGoal(string goalName, string goalDescription, int pointValue, int bonus, int target) : base(goalName, goalDescription, pointValue)
-     {
-          _current = 0;        
+     {        
           _bonus = bonus;
           _target = target;
      }
@@ -22,7 +21,7 @@ using System;
        //Constructor of ChecklistGoal for the RecordEvent method in GoalManager
      public ChecklistGoal(string goalName, string goalDescription, int pointValue, bool isComplete, int current, int target, int bonus) :base(goalName, goalDescription, pointValue)
      {
-          _current = 0;   
+          _current = current;   
           _target = target;
           _bonus = bonus;
      }
@@ -31,30 +30,45 @@ using System;
     //Override method of GetGoalString specific to ChecklistGoal
      public override string GetGoalString()
      {
-         return $"[{GetIsCompleteChar()} ] {_goalName} ({_goalDescription}) -- Currently completed: [{_current}/{_target}]";
+         return $"[{GetIsCompleteChar()} ] {_goalName} ({_goalDescription}) -- Goal Status: {_current}/{_target}";
      }
 
      //Override method of GetSaveString specific to ChecklistGoal
      public override string GetSaveString()
      {
-        return$"{GetType()}:{_goalName}|{_goalDescription}|{_pointValue}|{_bonus}|{_target}|{_current}|{_isComplete}";
+        return $"{GetType()}:{_goalName}|{_goalDescription}|{_pointValue}|{_bonus}|{_current}|{_target}|{_isComplete}";
      }
 
      //RecordEvent method implemented from abstract method of Goal class
       public override int RecordEvent()
       {
-        if (_current < _target)
+        if (_current < (_target - 1))
         {
+        //    _isComplete = true;
+        //     Console.WriteLine("I am here");
+        //     _pointValue += _bonus;
+        //     Console.WriteLine($"Congratluations! You have completed your Goal! You earned {_pointValue}!");
+        //     DisplayAnimation(); 
+
             _current++;
             Console.WriteLine($"Congratulations! You have earned {_pointValue}");
         }
-        else if (_current == _target)
+        else 
         {
+            //  _current++;
+            // Console.WriteLine($"Congratulations! You have earned {_pointValue}");
             _isComplete = true;
+            Console.WriteLine("I am here");
             _pointValue += _bonus;
             Console.WriteLine($"Congratluations! You have completed your Goal! You earned {_pointValue}!");
             DisplayAnimation();
         }
+
+        // while (_current <= _target)
+        // {
+        //     _current++
+        //     Console.WriteLine($"You have earned {_pointValue}. Keep Working toward completing your goal.");
+        // }
           
           
           

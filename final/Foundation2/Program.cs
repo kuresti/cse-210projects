d522a2819e8d10f8b3bdeb9635ee1da29f5abcbc
kuresti <ure17002@byui.edu>
2023-12-07 07:53:17 -0700

    Foundation 2

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -44,7 +44,7 @@ class Program
          
          Order ord2 = new Order(customer2);
 
-        //Adding Products to ord1
+        //Adding Products to ord2
          Product ord2prod1 = new Product("Ear Buds", 456712, 75.00m, 1);
                  ord2prod1.CalculatePrice();
                  ord2.AddProduct(ord2prod1);
@@ -63,14 +63,51 @@ class Program
 
         //Create line between orders
         Console.WriteLine("");
-        //Shipping label for ord1
+        //Shipping label for ord2
         ord2.GetShippingLabel();
 
-        //Packing label for ord1
+        //Packing label for ord2
         ord2.GetPackingLabel();
 
-        //Total Price for ord1
+        //Total Price for ord2
         ord2.CalculateTotalCost();
+
+        //Order Three Created
+         Address customer3Address = new Address("534 W 6789 S", "Boise", "Idaho", "USA");
+
+         Customer customer3 = new Customer("Kalare  Urrestilla", customer3Address);
+         
+         Order ord3 = new Order(customer3);
+
+        //Adding Products to ord3
+         Product ord3prod1 = new Product("Ear Buds", 456712, 75.00m, 1);
+                 ord3prod1.CalculatePrice();
+                 ord3.AddProduct(ord3prod1);
+               
+         Product ord3prod2 = new Product("Paella Pan", 000365, 96.00m, 2);
+                ord3prod2.CalculatePrice();
+                ord3.AddProduct(ord3prod2);
+            
+         Product ord3prod3 = new Product("Olive Oil", 467813, 18.00m, 3);
+                ord3prod3.CalculatePrice();
+                ord3.AddProduct(ord3prod3);
+
+         Product ord3prod4 = new Product("Blue Shirt", 783928, 45.00m, 2);
+                ord3prod4.CalculatePrice();
+                ord3.AddProduct(ord3prod4);
+
+        //Create line between orders
+        Console.WriteLine("");
+        //Shipping label for ord3
+        ord3.GetShippingLabel();
+
+        //Packing label for ord3
+        ord3.GetPackingLabel();
+
+        //Total Price for ord3
+        ord3.CalculateTotalCost();
+
+        
         
     }
 
